94fae35f3f93856b16feda013c6eb4dacdcc6afd
momonoki1990 <naoya-kuma3@hotmail.co.jp>
2021-12-07 09:18:34 +0900

    add chakraui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import List from "./components/List";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <List />
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <List />
+      </div>
+    </ChakraProvider>
   );
 }
 
